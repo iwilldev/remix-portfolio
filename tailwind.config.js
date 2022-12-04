f856060314c335d9ead3383610ff5bc7776c7aab
William Gonçalves <euwilliamgoncalves@gmail.com>
2022-12-04 06:10:45 -0300

    feat: add custom palette for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        will: {
+          50: "#FCE9ED",
+          100: "#F5BCC9",
+          200: "#EF8FA4",
+          300: "#E96380",
+          400: "#E2365C",
+          500: "#C91D42",
+          600: "#9C1633",
+          700: "#701025",
+          800: "#430A16",
+          900: "#160307",
+        }
+      }
+    },
   },
   plugins: [],
 };
